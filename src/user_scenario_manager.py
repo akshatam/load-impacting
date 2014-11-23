fc410ab94b5e11aeef4cf7a61610d947295dcd73
Amar Akshat <akshat@akshatam.in>
2014-11-24 01:35:31 +0900

    Introduce Sleep TIme.

--- src/user_scenario_manager.py
+++ src/user_scenario_manager.py
@@ -5,7 +5,7 @@ import os
 import config_manager
 
 class usr_scenario_mgr(object):
-    def __init__(self, path, configuration, batch_size=2):
+    def __init__(self, path, configuration, batch_size=2, sleep_time=15):
         self.file = None
         try:
             self.file = open(path, "w")
@@ -25,7 +25,8 @@ class usr_scenario_mgr(object):
 
                 self.file.write("\t{\"%s\", \"http://%s/%s\", %s\n" % (sp.method, sp.domain, sp.path, headers))
                 if batch_size_counter == batch_size:
-                    self.file.write("}),\n")
+                    self.file.write("})\n")
+                    self.file.write("client.sleep(%d)\n" % sleep_time)
                     if idx != len(configuration.http_sampler_proxies):
                         self.file.write("http.request_batch({\n")
                     batch_size_counter = 0
