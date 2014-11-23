e3dbd2ea6353ef60c76ec3b582d44dead433faf5
Amar Akshat <akshat@akshatam.in>
2014-11-23 23:57:19 +0900

    Scenario Manager, Extract the User Scenario script int a temporary file, in targets directory.

--- src/parser.py
+++ src/parser.py
@@ -2,11 +2,15 @@ import os
 import sys
 import loadimpact
 import config_manager
+import user_scenario_manager
 
 
 def main():
     c = config_manager.config_mgr()
     c.print_config()
+    path = "%s/target/tmp.txt" % (config_manager.PARENT_DIR)
+    usm = user_scenario_manager.usr_scenario_mgr(path, c, 2)
+    print usm
 
 
 if __name__ == '__main__':
--- src/user_scenario_manager.py
+++ src/user_scenario_manager.py
@@ -2,8 +2,45 @@ __author__ = 'Amar'
 
 import loadimpact
 import os
+import config_manager
 
 class usr_scenario_mgr(object):
-    def __init__(self):
-        pass
+    def __init__(self, path, configuration, batch_size=2):
+        self.file = None
+        try:
+            self.file = open(path, "w")
+            batch_size_counter = 1
+            self.file.write("http.request_batch({\n")
+            for idx, sp in enumerate(configuration.http_sampler_proxies):
+                headers = "{"
+                h_count = 0
+                for key in sp.map.keys():
+                    if h_count != 0:
+                        headers = headers + ","
+
+                    headers = headers + "[\"%s\"]=\"%s\"" % (key, sp.map[key])
+                    h_count += 1
+
+                headers = headers + "}"
+
+                self.file.write("\t{\"%s\", \"%s\", %s\n" % (sp.method, sp.path, headers))
+                if batch_size_counter == batch_size:
+                    self.file.write("}),\n")
+                    if idx != len(configuration.http_sampler_proxies):
+                        self.file.write("http.request_batch({\n")
+                    batch_size_counter = 0
+
+                if batch_size_counter != batch_size and idx == len(configuration.http_sampler_proxies) - 1:
+                    self.file.write("}),\n")
+
+
+                batch_size_counter += 1
+
+        except Exception as e:
+            print e
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.file != None:
+            self.file.close()
 
