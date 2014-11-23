f98698ca3349e934e34686d8add3d929064fc35d
Amar Akshat <akshat@akshatam.in>
2014-11-23 14:11:14 +0900

    Config Manager: Managing LI specific Configuration from JMX

---
+++ src/config_manager.py
@@ -0,0 +1,69 @@
+__author__ = 'Amar'
+
+import os
+import sys
+import xml.etree.ElementTree as ET
+
+PARENT_DIR = os.getcwd()
+if not os.path.isdir("%s/src" % (PARENT_DIR)):
+    print "Run from Project Root."
+    os.chdir(os.path.abspath(os.path.join(PARENT_DIR, os.pardir)))
+    PARENT_DIR = os.getcwd()
+
+class http_sampler_proxy(object):
+    def __init__(self, path, method):
+        self.path = path
+        self.method = method
+
+class config_mgr(object):
+    def __init__(self, filename=None):
+        if filename == None:
+            print "Using Default config JMX file"
+            filename = "%s/resources/jmeter_test.xml" % (PARENT_DIR)
+
+        self.filename = filename
+
+        try:
+            tree = ET.parse(self.filename)
+            root = tree.getroot()
+            ele = root.iter('ThreadGroup').next()
+            self.testname = ele.attrib['testname']
+
+            for conf in root.iter('stringProp'):
+                a = conf.get('name')
+                txt = conf.text
+                if a == 'ThreadGroup.num_threads':
+                    self.num_threads = int(txt)
+                elif a == 'ThreadGroup.ramp_time':
+                    self.ramp_time = int(txt)
+                elif a == 'HTTPSampler.domain':
+                    self.httpsampler_domain = txt
+                elif a == 'HTTPSampler.concurrentPool':
+                    self.httpsampler_concurrent_pool = int(txt)
+                else:
+                    pass
+
+            self.http_sampler_proxies = []
+            for conf in root.iter('HTTPSamplerProxy'):
+                path = None
+                method = None
+                for str in conf.iter('stringProp'):
+                    if str.get('name') == 'HTTPSampler.path':
+                        path = str.text
+                    elif str.get('name') == 'HTTPSampler.method':
+                        method = str.text
+                hsp = http_sampler_proxy(path, method)
+                self.http_sampler_proxies.append(hsp)
+
+        except Exception as e:
+            print e
+
+    def print_config(self):
+        print "ThreadGroup.num_threads:"    + str(self.num_threads)
+        print "ThreadGroup.ramp_time:"      + str(self.ramp_time)
+        print "ThreadGroup.domain:"         + str(self.httpsampler_domain)
+        print "ThreadGroup.concurrent_pool" + str(self.httpsampler_concurrent_pool)
+
+        for i in self.http_sampler_proxies:
+            print "HTTP Sampler Path:"   + i.path
+            print "HTTP Sampler Method:" + i.method
--- src/parser.py
+++ src/parser.py
@@ -1,35 +1,12 @@
 import os
 import sys
 import loadimpact
-import xml.etree.ElementTree as ET
+import config_manager
 
 
 def main():
-    PARENT_DIR = os.getcwd()
-    if not os.path.isdir("%s/src" % (PARENT_DIR)):
-        print "Run from Project Root."
-        os.chdir(os.path.abspath(os.path.join(PARENT_DIR, os.pardir)))
-        PARENT_DIR = os.getcwd()
-
-    print "Parent DIR is %s" % (PARENT_DIR)
-
-    RESOURCES_DIR = "%s/resources" % (PARENT_DIR)
-    try:
-        tree = ET.parse("%s/jmeter_test.xml" % (RESOURCES_DIR))
-        root = tree.getroot()
-        ele = root.iter('ThreadGroup').next()
-        name = ele.attrib['testname']
-        print name
-        for a in ele.iter('strinProp'):
-            print a
-
-        for conf in root.iter('stringProp'):
-            a = conf.get('name')
-            print a
-            print conf.text
-
-    except Exception as e:
-        print e
+    c = config_manager.config_mgr()
+    c.print_config()
 
 
 if __name__ == '__main__':
